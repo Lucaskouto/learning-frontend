e7e93ee2f13bb6f7f664d58f07981ee941e07c01
Lucas Couto <coutodesouzalucas@gmail.com>
2022-09-26 20:59:22 -0300

    object notion

---
+++ fundamento/object.js
@@ -0,0 +1,17 @@
+// Objeto - Anotação literal de objetos
+// Json - Javascript object notation - não é um object
+// Um objeto é uma coleção de chave/valor
+
+const prod1 = {}; // Par de chaves - representa um objeto
+prod1.nome = 'Celular Ultra Mega';
+prod1.preco = 4998.90;
+prod1['Desconto Legal'] = 0.40; // Evitar atributos com espaço
+
+console.log(prod1);
+
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 79.90,
+}
+
+console.log(prod2);
