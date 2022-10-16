ef85896f4dc9c6b28770af231201327daa9cfad2
Lucas Couto <coutodesouzalucas@gmail.com>
2022-10-15 21:41:15 -0300

    point notation

--- fundamento/notacaoPonto.js
+++ fundamento/notacaoPonto.js
@@ -1,21 +1,27 @@
-console.log(Math.ceil(6.1)) // Arredondamento para cima
+// #Notação Ponto
+
+console.log(Math.ceil(6.1)); // notação ponto usado para acessar uma função dentro de Math
 
 const obj1 = {}
-obj1.nome = 'Bola'
-// obj1['nome'] = 'Bola2'
-console.log(obj1.nome)
+obj1.nome = 'Espada' // Adicionar um nome/valor dentro do Objeto obj1
+// obj1['nome'] = 'Lança' // mesma forma de escrever 
+// não é recomendado usar este método, pois não tera como acessar o objeto com notação ponto futuramente no código
+
+console.log(obj1.nome);
 
 function Obj(nome) {
-    this.nome = nome
+    this.nome = nome // this.nome recebe nome e ficara visivel globalmente
     this.exec = function() {
-        console.log('Exec...')
+    console.log('Exec...')
     }
 }
 
-const obj2 = new Obj('Cadeira')
-const obj3 = new Obj('Mesa')
+
+const obj2 = new Obj('Machado')
+const obj3 = new Obj('Cajado')
+
+console.log(obj1.nome)
 console.log(obj2.nome)
 console.log(obj3.nome)
 obj3.exec()
 
-// notação ponto está presente na linguagem como um todo
