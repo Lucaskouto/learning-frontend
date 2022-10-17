e46ebd4d4db70f34688756da1a2687e9d57ce6fc
Lucas Couto <coutodesouzalucas@gmail.com>
2022-10-17 19:39:41 -0300

    Operadores Aritmeticos

---
+++ fundamento/aritmeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [2, 3, 20, 5];
+
+const soma = a + b
+const subtracao = b - a
+const divisao = c / a
+const multiplicacao = d * a
+const modular = c % 4
+
+console.log(soma, subtracao, divisao, multiplicacao, modular);
+
+// Operadores binarios -> a + b, b - a...
+// Operadores  unários -> -a +b
